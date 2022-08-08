6ae7b479c80c2fd3f32c24059234f011ffe25f27
AMAN RAJ <80095483+AMAN55555@users.noreply.github.com>
2022-08-08 08:35:23 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,4 @@
 module.exports = {
-    MONGO_URI : "mongodb+srv://Aman:%402001@cluster0.mvejycs.mongodb.net/?retryWrites=true&w=majority",
-    JWT: 'lmao-noob'
+    MONGO_URI : "",
+    JWT: ''
 }
-//mongodb+srv://root:<password>@blogfeed.qekka.mongodb.net/blogfeed?retryWrites=true&w=majority
-//mongodb://root:blogfeed1234@ds339648.mlab.com:39648/blogfeed
